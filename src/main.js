788ca31ce3732684b720569f0fdec7e1addfddd0
summer <zxycom2018@outlook.com>
2021-04-18 20:29:11 +0800

    xiugai

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//字体图标
 import "./assets/font/iconfont.css"
 // rem 适配
 import "amfe-flexible"
